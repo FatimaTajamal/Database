59c04278d7a5dd42102534b5710eee73c0013f0a
Fatima Tajamal <fatimatajamal1@gmail.com>
2025-12-08 18:36:08 +0500

    Routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const { adminLogin, verifyToken } = require('../controllers/adminController');
 const { authenticateToken } = require('../middleware/auth');
 
+
+router.post('/test-post', (req, res) => { 
+  res.json({ debug: 'POST test received!' });
+});
 // Public routes
 router.post('/login', adminLogin);
+
 
 // Protected routes
 router.get('/verify', authenticateToken, verifyToken);
